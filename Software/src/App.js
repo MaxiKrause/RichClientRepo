25f72e66afe1214d197f7a01932f053a99a65061
Maximilian Krause <maximilian.krause@fh-bielefeld.de>
2018-06-07 11:00:55 +0200

    added Software

---
+++ Software/src/App.js
@@ -0,0 +1,84 @@
+import React, { Component } from 'react';
+import { Navbar, Button } from 'react-bootstrap';
+import './App.css';
+
+class App extends Component {
+  componentWillMount() {
+    this.setState( {profile : {}});
+    const { userProfile, getProfile } = this.props.auth;
+    if (!userProfile) {
+      getProfile((err, profile) => {
+        this.setState({ profile });
+      });
+    } else {
+      this.setState({ profile: userProfile });
+    }
+
+  }
+
+  goTo(route) {
+    this.props.history.replace(`/${route}`)
+  }
+
+  login() {
+    this.props.auth.login();
+  }
+
+  logout() {
+    this.props.auth.logout();
+  }
+
+  render() {
+    const { isAuthenticated } = this.props.auth;   
+    const { profile } = this.state;
+    console.log(this.state);
+    return (
+      <div>
+        <Navbar fluid>
+          <Navbar.Header>
+            <Navbar.Brand>
+              <a href="#">Auth0 - React</a>
+            </Navbar.Brand>
+            <Button
+              bsStyle="primary"
+              className="btn-margin"
+              onClick={this.goTo.bind(this, 'home')}
+            >
+              Home
+            </Button>
+            {
+              !isAuthenticated() && (
+                  <Button
+                    id="qsLoginBtn"
+                    bsStyle="primary"
+                    className="btn-margin"
+                    onClick={this.login.bind(this)}
+                  >
+                    Log In
+                  </Button>
+                )
+            }
+            {
+              isAuthenticated() && (
+                  <Button
+                    id="qsLogoutBtn"
+                    bsStyle="primary"
+                    className="btn-margin"
+                    onClick={this.logout.bind(this)}
+                  >
+                    Log Out
+                  </Button>
+                  
+               )
+            }
+            <Navbar.Text>
+              Hallo { profile !== 0 && profile.nickname}
+            </Navbar.Text>
+          </Navbar.Header>
+        </Navbar>
+      </div>
+    );
+  }
+}
+
+export default App;
