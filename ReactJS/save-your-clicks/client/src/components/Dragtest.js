89b6256a23c06240aa45295d0ee5d2fad51e2df2
Dirk <dirk.siemens@fh-bielefeld.de>
2018-06-14 12:38:36 +0200

    Lock button added

--- ReactJS/save-your-clicks/client/src/components/Dragtest.js
+++ ReactJS/save-your-clicks/client/src/components/Dragtest.js
@@ -8,88 +8,53 @@ import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import './Dragtest.css';
 
-const styles = {
-  
-};  
+let drag=false;  
+
+function clearSelection() {
+    if (window.getSelection) {
+        window.getSelection().removeAllRanges();
+    } else if (document.selection) {
+        document.selection.empty();
+    }
+}
+
+function DragCard(props){
+    return (
+    <Draggable grid={[25, 25]} bounds="body" {...props}>
+              <div>
+                <Card className="card">
+                  <CardContent>
+                    <Typography className="title" color="textSecondary">
+                      Word of the Day
+                    </Typography>
+                    <Typography variant="headline" component="h2">
+                      YEET
+                    </Typography>
+                    <Typography className="pos" color="textSecondary">
+                      adjective
+                    </Typography>
+                    <Typography component="p">
+                      well meaning and kindly.<br />
+                      {'"a benevolent smile"'}
+                    </Typography>
+                  </CardContent>
+                  <CardActions>
+                    <Button size="small" onClick={() => { props.disabled=true }}>Learn More</Button>
+                  </CardActions>
+                </Card>
+              </div>
+            </Draggable>
+  );
+  }
 
 class Dragtest extends React.Component {
 
- 	
- 	render() {
+ 	render(props) {
  				return(
 		 			<div style={{ width: 500 }}>
-		 				<Draggable grid={[25, 25]} bounds="body">
-              <div>
-                <Card className="card">
-                  <CardContent>
-                    <Typography className="title" color="textSecondary">
-                      Word of the Day
-                    </Typography>
-                    <Typography variant="headline" component="h2">
-                      YEET
-                    </Typography>
-                    <Typography className="pos" color="textSecondary">
-                      adjective
-                    </Typography>
-                    <Typography component="p">
-                      well meaning and kindly.<br />
-                      {'"a benevolent smile"'}
-                    </Typography>
-                  </CardContent>
-                  <CardActions>
-                    <Button size="small">Learn More</Button>
-                  </CardActions>
-                </Card>
-              </div>
-            </Draggable>
-            <Draggable grid={[25, 25]} bounds="body">
-              <div>
-                <Card className="card">
-                  <CardContent>
-                    <Typography className="title" color="textSecondary">
-                      Word of the Day
-                    </Typography>
-                    <Typography variant="headline" component="h2">
-                      YEET
-                    </Typography>
-                    <Typography className="pos" color="textSecondary">
-                      adjective
-                    </Typography>
-                    <Typography component="p">
-                      well meaning and kindly.<br />
-                      {'"a benevolent smile"'}
-                    </Typography>
-                  </CardContent>
-                  <CardActions>
-                    <Button size="small">Learn More</Button>
-                  </CardActions>
-                </Card>
-              </div>
-            </Draggable>
-            <Draggable grid={[25, 25]} bounds="body">
-              <div>
-                <Card className="card">
-                  <CardContent>
-                    <Typography className="title" color="textSecondary">
-                      Word of the Day
-                    </Typography>
-                    <Typography variant="headline" component="h2">
-                      YEET
-                    </Typography>
-                    <Typography className="pos" color="textSecondary">
-                      adjective
-                    </Typography>
-                    <Typography component="p">
-                      well meaning and kindly.<br />
-                      {'"a benevolent smile"'}
-                    </Typography>
-                  </CardContent>
-                  <CardActions>
-                    <Button size="small">Learn More</Button>
-                  </CardActions>
-                </Card>
-              </div>
-            </Draggable>
+		 				<DragCard disabled={drag}/>
+            <DragCard disabled={drag}/>
+            <Button size="small" onClick={() => { (drag===true) ? drag=false : drag=true ;  clearSelection(); this.forceUpdate() }}>Learn More</Button>
 		 			</div>
 	 			)
  	}
