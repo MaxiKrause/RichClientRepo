6f498b4fcdf090d1f962bee97c4254b96fb79e30
Dirk <dirk.siemens@fh-bielefeld.de>
2018-06-14 11:29:58 +0200

    Grenzen hinzugefügt

--- ReactJS/save-your-clicks/client/src/components/Dragtest.js
+++ ReactJS/save-your-clicks/client/src/components/Dragtest.js
@@ -6,6 +6,7 @@ import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
+import './Dragtest.css';
 
 const styles = {
   
@@ -17,7 +18,55 @@ class Dragtest extends React.Component {
  	render() {
  				return(
 		 			<div style={{ width: 500 }}>
-		 				<Draggable grid={[25, 25]}>
+		 				<Draggable grid={[25, 25]} bounds="body">
+              <div>
+                <Card className="card">
+                  <CardContent>
+                    <Typography className="title" color="textSecondary">
+                      Word of the Day
+                    </Typography>
+                    <Typography variant="headline" component="h2">
+                      YEET
+                    </Typography>
+                    <Typography className="pos" color="textSecondary">
+                      adjective
+                    </Typography>
+                    <Typography component="p">
+                      well meaning and kindly.<br />
+                      {'"a benevolent smile"'}
+                    </Typography>
+                  </CardContent>
+                  <CardActions>
+                    <Button size="small">Learn More</Button>
+                  </CardActions>
+                </Card>
+              </div>
+            </Draggable>
+            <Draggable grid={[25, 25]} bounds="body">
+              <div>
+                <Card className="card">
+                  <CardContent>
+                    <Typography className="title" color="textSecondary">
+                      Word of the Day
+                    </Typography>
+                    <Typography variant="headline" component="h2">
+                      YEET
+                    </Typography>
+                    <Typography className="pos" color="textSecondary">
+                      adjective
+                    </Typography>
+                    <Typography component="p">
+                      well meaning and kindly.<br />
+                      {'"a benevolent smile"'}
+                    </Typography>
+                  </CardContent>
+                  <CardActions>
+                    <Button size="small">Learn More</Button>
+                  </CardActions>
+                </Card>
+              </div>
+            </Draggable>
+            <Draggable grid={[25, 25]} bounds="body">
               <div>
                 <Card className="card">
                   <CardContent>
