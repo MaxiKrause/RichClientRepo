995039e6306b50c2628f90057575409c9f2d222d
unknown <siemens.dirk@web.de>
2018-07-13 12:24:23 +0200

    YTCard added

--- ReactJS/save-your-clicks/client/src/components/Home.js
+++ ReactJS/save-your-clicks/client/src/components/Home.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import DragCard from './DragCard.js';
 import NewsCard from './NewsCard.js';
+import YTCard from './YTCard.js';
 
 class Home extends Component {
 	
@@ -12,6 +13,7 @@ class Home extends Component {
     			<p>Home-Seite</p>
     			<DragCard grid={[25, 25]} bounds="body"/>
     			<NewsCard grid={[25, 25]} bounds="body"/>
+    			<YTCard grid={[25, 25]} bounds="body"/>
   			</div>
   		);
 	}
---
+++ ReactJS/save-your-clicks/client/src/components/YTCard.js
@@ -0,0 +1,149 @@
+import React from 'react';
+import Draggable from 'react-draggable';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader'
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import LockIcon from '@material-ui/icons/Lock';
+import UnlockIcon from '@material-ui/icons/LockOpen';
+
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogTitle from '@material-ui/core/DialogTitle';
+
+import TextField from '@material-ui/core/TextField';
+import GetYTID from 'get-youtube-id'
+
+class YTCard extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			disabled : false,
+			dialogOpen: false,
+			link: "https://www.youtube.com/",
+			embedlink: "",
+			id: "",
+		};
+	}
+
+	getID(ytlink) {
+		let ytid = GetYTID(ytlink);
+		this.setState({id: ytid})
+		console.log(ytid)
+	}
+
+	clearSelection() {
+	    if (window.getSelection) {
+	        window.getSelection().removeAllRanges();
+	    } else if (document.selection) {
+	        document.selection.empty();
+	    }
+	}
+
+	changeMoveState(e) {
+		e.preventDefault();
+    	e.stopPropagation();
+    	this.clearSelection();
+    	const {disabled} = this.state;
+    	this.setState({disabled: !disabled});
+	}
+
+	
+	handleDialogOpen() {
+		this.setState({
+			dialogOpen: true,
+			disabled: true
+		});
+	}
+
+	handleDialogClose(event) {
+		this.setState({dialogOpen: false});
+	}
+
+	handleChangeInput(event) {
+    	this.setState({ link: event.target.value });
+    	console.log(this.state.link)
+
+		this.getID(this.state.link)
+
+		
+    	let embid = "https://www.youtube.com/embed/"+this.state.id;
+
+    	this.setState({ embedlink: embid})
+  	};
+
+	componentDidMount() {
+
+	}
+
+	render() {
+		return (
+		    <Draggable disabled={this.state.disabled} {...this.props}>
+	        	<div style={{ width: 500 }}>
+	        		<Card className="card">
+	        			<CardHeader 
+	        				action={
+	        					<div>
+		        					<IconButton size="small" onClick={this.changeMoveState.bind(this)}>
+		        						{
+		    								this.state.disabled && (
+		    									<LockIcon />
+		    								)
+		    							}
+		    							{
+		    								!this.state.disabled && (
+		    									<UnlockIcon />
+		    								)
+		    							}
+	      							</IconButton>
+	      					        <IconButton onClick={this.handleDialogOpen.bind(this)}>
+	                					<MoreVertIcon />
+	              					</IconButton>
+	              				</div>
+	        				}
+	        			/>
+	        			<CardContent>
+	                		<iframe width="420" height="315"
+								src={this.state.embedlink}>
+							</iframe>	                		
+	              		</CardContent>
+	              		<CardActions>
+	              		</CardActions>
+	            	</Card>
+	            	<Dialog
+	            		open={this.state.dialogOpen}
+          			>
+          				<div>
+          					<DialogTitle>
+          						Einstellungen
+          					</DialogTitle>
+          					<DialogContent>
+	            				<TextField
+	            					id="link" 
+	            					value={this.state.link} 
+	            					onInput={this.handleChangeInput.bind(this)} 
+	            					label="Youtube Link"
+	            				/>
+	            			</DialogContent>
+	    					<DialogActions>
+	    						<Button onClick={() => this.setState({dialogOpen: false})} color="primary" id="cancel">
+					            	Abbrechen
+					            </Button>
+					            <Button onClick={this.handleDialogClose.bind(this)} color="primary" id="save">
+					            	Speichern
+					            </Button>
+	    					</DialogActions>
+          				</div>
+          			</Dialog>
+	          	</div>
+	        </Draggable>
+		);
+	}
+}
+
+export default YTCard;
