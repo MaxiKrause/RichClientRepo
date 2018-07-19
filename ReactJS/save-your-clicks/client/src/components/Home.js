7f2ed943c3cdc6912e62e9590e037b5182545500
Maximilian Krause <kane15_96@yahoo.de>
2018-07-19 18:39:32 +0200

    einladen der Widgets klappt jetzt

--- ReactJS/save-your-clicks/client/src/components/Home.js
+++ ReactJS/save-your-clicks/client/src/components/Home.js
@@ -2,9 +2,21 @@ import React, { Component } from 'react';
 import DragCard from './DragCard.js';
 import NewsCard from './NewsCard.js';
 import YTCard from './YTCard.js';
+import {generateElement} from 'react-live';
+import Draggable from 'react-draggable';
+import Card from '@material-ui/core/Card';
 
 class Home extends Component {
-	
+	constructor(props) {
+		super(props);
+		let list = [];
+		list.push(generateElement({code: "<div>hallo</div>"}));
+		this.state = {
+			components: list,
+			loaded: false
+		};
+	}
+
 	componentDidMount() {
 		const id = this.props.auth.getProfile((err, profile) => {
 			const userid = profile.sub;
@@ -14,23 +26,29 @@ class Home extends Component {
 				.then((data) => this.handleComponents(data));
 		})
 
-		
 	}
 
 	handleComponents(data) {
-		console.log(data);
+		let list = [];
+		console.log(data[0].component);
+		for (var i = data.length - 1; i >= 0; i--) {
+			list.push(Element = generateElement({code: data[i].component, scope:{Draggable, Card}}));
+		}
+		this.setState({
+			components : list,
+		});
 	}
 
 	render() {
-
-
   		return(
   			<div>
     			<p>Home-Seite</p>
     			<DragCard grid={[25, 25]} bounds="body"/>
     			<NewsCard grid={[25, 25]} bounds="body"/>
     			<YTCard grid={[25, 25]} bounds="body"/>
+    			{this.state.components.map((Components) => <Components />)}
   			</div>
+
   		);
 	}
 }
