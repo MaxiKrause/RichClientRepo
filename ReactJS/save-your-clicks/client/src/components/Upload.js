1adb91b062dfac8f80c5730d9ff78a0ce99adf71
unknown <siemens.dirk@web.de>
2018-09-25 14:20:25 +0200

    Changed the React Live Component

--- ReactJS/save-your-clicks/client/src/components/Upload.js
+++ ReactJS/save-your-clicks/client/src/components/Upload.js
@@ -3,6 +3,12 @@ import './Upload.css';
 import Button from '@material-ui/core/Button'
 import Draggable from 'react-draggable';
 import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import IconButton from '@material-ui/core/IconButton';
+import MoreVertIcon from '@material-ui/icons/MoreVert';	
+import TextField from '@material-ui/core/TextField';
+import LockIcon from '@material-ui/icons/Lock';
+import UnlockIcon from '@material-ui/icons/LockOpen';
 import {
   LiveProvider,
   LiveEditor,
@@ -18,18 +24,138 @@ class Upload extends Component {
     const div = generateElement({code : '<div> </div>'});
     const startCode = 	
     `class DragCard extends React.Component {
-		render() {
-			return (
-				<Draggable>
-					<Card>
-						<div>
-							Hello World!
-						</div>
-					</Card>
-				</Draggable>
-			);
-		}
-	}`;
+    
+    constructor(props) {
+      super(props);
+      this.state = {
+	      disabled: true,
+	      sdata: "",
+      };
+    }
+
+      handleData(data) {
+      this.setState({sdata: data});
+    }
+
+      fetchData() {	
+
+      	const titlesearch = "t="+this.state.title;
+
+      const newsAPI = "https://omdbapi.com/?"+titlesearch;
+      const API_KEY = "1d30d952";
+
+	    fetch(newsAPI + "&plot=full&apikey=" + API_KEY)
+	      .then((response) => response.json())
+	      .then((data) => this.handleData(data));
+    } 
+
+
+	handleDialogClose(event) {
+		this.fetchData();
+	}
+
+	handleChangeTitle(event) {
+    	this.setState({ title: event.target.value });
+  	};
+
+  	clearSelection() {
+	    if (window.getSelection) {
+	        window.getSelection().removeAllRanges();
+	    } else if (document.selection) {
+	        document.selection.empty();
+	    }
+	}
+
+  	changeMoveState(e) {
+		e.preventDefault();
+    	e.stopPropagation();
+    	this.clearSelection();
+    	const {disabled} = this.state;
+    	this.setState({disabled: !disabled});
+	}
+
+    render() {
+      return (
+        <Draggable disabled={this.state.disabled}>
+          <Card>
+          <CardHeader 	             				
+	        action={
+	        	<div>
+		            <IconButton size="small" onClick={this.changeMoveState.bind(this)}>
+		        		{
+		    				this.state.disabled && (
+		    					<LockIcon />
+		    				)
+		    			}
+		    			{
+		    				!this.state.disabled && (
+		    					<UnlockIcon />
+		    				)
+		    			}
+	      			</IconButton>
+	        	</div>
+	        }
+	      />
+            <div>
+            	<TextField 
+	            	id="title" 
+	            	onChange={this.handleChangeTitle.bind(this)} 
+	            	label="Filmtitel"
+	            />
+            	<Button onClick={this.handleDialogClose.bind(this)} color="primary" id="save">
+			      	Senden
+			    </Button>
+
+		        <TextField
+		          label="Erscheinungsjahr"
+		          InputLabelProps={{
+		            shrink: true,
+		          }}
+		          placeholder="Erscheinungsjahr"
+		          value={this.state.sdata.Year}
+		          margin="normal"
+		        />
+
+		        <TextField
+		          label="Genre"
+		          InputLabelProps={{
+		            shrink: true,
+		          }}
+		          placeholder="Genre"
+		          value={this.state.sdata.Genre}
+		          fullWidth
+		          margin="normal"
+		        />
+
+		        <TextField
+		          label="Handlung"
+		          InputLabelProps={{
+		            shrink: true,
+		          }}
+		          placeholder="Handlung"
+		          value={this.state.sdata.Plot}
+		          multiline
+         		  rowsMax="9"
+		          fullWidth
+		          margin="normal"
+		        />
+
+		        <TextField
+		          label="Bewertung"
+		          InputLabelProps={{
+		            shrink: true,
+		          }}
+		          placeholder="Bewertung"
+		          value={this.state.sdata.imdbRating}
+		          margin="normal"
+		        />
+            </div>
+          </Card>
+
+        </Draggable>
+      );
+    }
+  }`;
     this.state = {
 			content: startCode,
 			component: div,
@@ -79,8 +205,9 @@ class Upload extends Component {
  					)
  				}
 
- 				<LiveProvider code={this.state.code} scope={{Draggable, Card, React}}>
-			      <LiveEditor className="Editor" onChange={this.handleContent.bind(this)} />
+ 				<LiveProvider code={this.state.code} scope={{Draggable, Card, React, CardHeader, IconButton, MoreVertIcon, TextField, Button
+ 															,UnlockIcon, LockIcon}}>
+			      <LiveEditor className="Editor"  />
 			      <LiveError />
 			      <LivePreview className="Preview"/>
 			    </LiveProvider>
